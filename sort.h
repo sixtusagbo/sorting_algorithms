0e5a3f89549866a83b7c763a6e6bc264a1c9b84a
sixtusagbo <miracleagbosixtus@gmail.com>
2023-01-24 20:27:43 +0100

    [Feature] Cocktail shaker sort
    Extends bubble sort by operating in two directions

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void quick_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
+void cocktail_sort_list(listint_t **list);
 
 #endif
