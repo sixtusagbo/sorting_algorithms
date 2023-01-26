be060280f9bf6b744f7cc87b3c768cf89b70e839
sixtusagbo <miracleagbosixtus@gmail.com>
2023-01-26 19:14:30 +0100

    [Feature] Counting sort

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void _qsort(int *a, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif
